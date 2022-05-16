85e663e9f9d9abeeb99e03f0b9dd6fc84a386ef3
Senior_DEV <86672166+CJpaekil@users.noreply.github.com>
2022-05-16 11:13:06 +0900

    all updated

--- angular_test/test/frontend/compatibility_test.dart
+++ angular_test/test/frontend/compatibility_test.dart
@@ -21,7 +21,7 @@ void main() {
   });
 
   tearDown(disposeAnyRunningTest);
-
+sdfsfsfsdf
   test('createTestInjector should support SlowComponentLoader', () async {
     final injector = createTestInjector([]);
     final loader = injector.provideType<SlowComponentLoader>(
